878821d2099e194a8f00c50fbb7666d96c92b55d
Lucas <ivankiolucas@gmail.com>
2020-10-05 16:44:42 -0300

    ADC and PWM added to the project.

--- BBBProject/main.cpp
+++ BBBProject/main.cpp
@@ -1,27 +1,41 @@
 #include <iostream>
+#include <unistd.h>
 #include "BlackGPIO.h"
 #include "BlackTime.h"
+#include "BlackPWM.h"
+#include "BlackADC.h"
 
 using namespace BlackLib;
 using namespace std;
 
 int main(void) {
 
-	BlackGPIO PINx(GPIO_51, output, SecureMode);
+	
 	BlackTime myTime(8,12,40);
+	BlackPWM myPWM(P9_14);
+	BlackGPIO PINx(GPIO_51, output, SecureMode);
+	BlackADC myADC(AIN5);
+
+	myPWM.setDutyPercent(0);
 
 	myTime.start();
 	BlackTimeElapsed et = myTime.restart();
 	int lastSec, currentSec;
-	lastSec = et.second;
-	PINx.setValue(high);
+	lastSec = et.miliSecond;
+	PINx.setValue(low);
 	for (;;) {
 		et = myTime.elapsed();
-		currentSec = et.second;
-		if (currentSec != lastSec) {
-			lastSec = currentSec;
+		currentSec = et.miliSecond;
+		if (currentSec>500) {
+			lastSec += currentSec;
 			PINx.toggleValue();
-			cout << "elapsed second :" << et.second << endl;
+			myPWM.setDutyPercent(myADC.getNumericValue() / 18);
+			cout << "elapsed milisecond :" << et.miliSecond << endl;
+			cout << "PWM duty cycle :" << myPWM.getValue() << endl;
+			et = myTime.restart();
 		}
+		
+		//cout << "ADC Value: " << myADC.getNumericValue() << endl;
+		//usleep(1000);
 	}
 }
